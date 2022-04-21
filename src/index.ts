a12d4d6009f0ef00aee0936534de4ccf847aa00e
Parth-Mac <parthkj6@gmail.com>
2022-04-21 15:47:18 -0400

    added angular

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
-console.log('Hello!');
-console.log('hi');
-console.log('hi');
+var funcs = [];
+for (var i = 0; i < 5; i += 1) {
+  var y = i;
+  funcs.push(function () {
+    console.log(y);
+  });
+}
+funcs.forEach(function (func) {
+  func();
+});
