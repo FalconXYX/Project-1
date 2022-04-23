5c218b24a8e4023226c66b4cfa4fe146c26a7c4e
Parth-Mac <parthkj6@gmail.com>
2022-04-22 21:17:43 -0400

    Build

--- dist/out-tsc/index.js
+++ dist/out-tsc/index.js
@@ -1,9 +1,39 @@
-function greeter(fn) {
-    fn("Hello, World");
+function main() {
+    let posarray = [];
+    let gamehappening = true;
+    while (gamehappening) {
+        posarray.push(picknext());
+        output(posarray);
+        gamehappening = checkanswer(posarray);
+    }
 }
 function picknext() {
     let i = Math.floor(Math.random() * 4) + 1;
-    console.log(i);
+    return i;
 }
-picknext();
+function getinput() {
+    let num = 0;
+    return num;
+}
+function checkanswer(parray) {
+    let posarray = parray;
+    for (let i = posarray.length; i > 0; i--) {
+        let inputnum = getinput();
+        if (inputnum != posarray[i]) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+}
+function output(parray) {
+    let posarray = parray;
+    for (let i = posarray.length; i > posarray.length; i++) {
+        flashpart(posarray[i]);
+    }
+}
+function flashpart(part) {
+    return;
+}
 //# sourceMappingURL=index.js.map
